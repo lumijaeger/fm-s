788ae56ab902e6b21e6acb6fa5daef6c2060e1ba
Ra <rarodriguezrosado@gmail.com>
2023-05-08 12:33:04 +0200

    GBCN-images 1.5.2

--- GBCN-images.user.js
+++ GBCN-images.user.js
@@ -1,9 +1,9 @@
 /*
 ==UserScript==
-@name           GBCN-images 5.1
+@name           GBCN-images
 @match          https://*.gbcnmedia.net/*.jpg
 @match          http://*.girlsbcn.net/*.jpg
-@version        4.3
+@version        1.5.2
 ==/UserScript==
 */
 
@@ -13,8 +13,9 @@
 
 let dire = window.location.href.replace(/fotos(\d)\.girlsbcn(.*)/,
   (match, p1, p2) => {
-    if (match) window.location.assign(`https://media${p1}.gbcnmedia${p2}`);});
-let re = /(?<dire>.*?.net\/(?<nombre>[a-zA-Z]*)\d{0,3}\/)(?<foto>(?:\k<nombre>)?\d{1,3})(?<ext>.*)/;
+    if (match) window.location.assign(`https://media${p1}.gbcnmedia${p2}`);
+  });
+let re = /(?<dire>.*?.net\/(?<nombre>[a-zA-Z]*)\d{0,3}\/)(?<foto>(?:\k<nombre>)?(?<numFoto>\d{1,3}))(?<ext>.*)/;
 let matches = dire.match(re);
 let photos = [];
 
@@ -25,54 +26,56 @@ if ((/0?01/).test(matches.groups.foto) < 3) {
 let style = document.createElement('style');
 style.appendChild(document.createTextNode(`
 body {
-	display: flex;
-	flex-flow: row wrap;
-	justify-content: center;
-	align-items: stretch;
-	align-content: stretch;
-	margin-block: 3vh;
-	gap: 1vh 1vw;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: stretch;
+  align-content: stretch;
+  margin-block: 3vh;
+  gap: 1vh 1vw;
+  scroll-snap-type: y proximity;
 }
 img {
-	position: initial;
-	object-fit: contain;
-	margin: 0;
-	max-height: 95vh;
-	flex: 1 1 auto;
+  position: initial;
+  object-fit: contain;
+  margin: 0;
+  max-height: 95vh;
+  flex: 1 1 auto;
+  scroll-snap-align: center;
 }
 img.landscape {
-	max-width: initial;
+  max-width: initial;
 }
 .original {
-	height: initial;
-	width: initial;
-	max-height: initial;
-	max-width: initial;
-	flex: 0 0 auto;
+  height: initial;
+  width: initial;
+  max-height: initial;
+  max-width: initial;
+  flex: 0 0 auto;
   align-self: center;
 }
 .peque {
-	height: 75vh;
-	max-height: initial;
-	max-width: initial;
-	flex: 0
+  height: 75vh;
+  max-height: initial;
+  max-width: initial;
+  flex: 0
 }
 `));
 
 (function addImg(pad = 2, malas = 0, i = 1) {
-  console.log({
-    pad,
-    malas,
-    i
-  });
   let direCompleta = `${dire}${(i.toString().padStart(pad,0))}.jpg`;
-
   let request = new XMLHttpRequest();
   request.open('GET', direCompleta, false);
 
   request.onreadystatechange = function() {
     if (request.readyState !== 4 || request.status !== 200) {
-      malas++;
+      if (pad === 2 || i > matches.groups.numFoto) malas++;
+      console.log({
+        direCompleta,
+        pad,
+        malas,
+        i
+      });
       return;
     }
     let img = document.createElement("img");
@@ -81,7 +84,7 @@ img.landscape {
       let targetClassList = e.target.classList;
       targetClassList[(targetClassList.contains("original") ? "remove" : "add")]("original");
     });
-		photos = [...photos, img]
+    photos = [...photos, img];
   };
 
   request.send();
@@ -100,8 +103,8 @@ document.title = photos.length + " " + matches.groups.nombre;
 while (document.body.childElementCount > 0) document.body.childNodes[0].remove();
 document.head.appendChild(style);
 photos.forEach((e) => {
-	document.body.appendChild(e);
-	// if (e.height < e.width) {
-	// 	e.classList.add("landscape");
-	// }
+  document.body.appendChild(e);
+  // if (e.height < e.width) {
+  //   e.classList.add("landscape");
+  // }
 });
