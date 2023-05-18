924e4659042bc655bd271f88ee84344f230d2aab
lumisjaeger <lumisjaeger>
2023-05-18 12:12:22 +0200

    SLUMI-busquedaVacia: fix wrong element search

--- SLUMI-busquedaVacia.user.js
+++ SLUMI-busquedaVacia.user.js
@@ -3,8 +3,8 @@
 @name           SLUMI-busquedaVacia
 @match          https://www.slumi.com/escorts/espa%C3%B1a/buscar.*
 @match          https://www.slumi.com/*
-@version        1.0
+@version        1.0.1
 ==/UserScript==
 */
 
-if (document.getElementsByClassName('sin_resultados')[0].style.display !== 'none' || document.getElementsByClassName('no_encontrado').length > 0) window.close();
+if (document.getElementsById('sin_resultados')[0].style.display !== 'none' || document.getElementsByClassName('no_encontrado').length > 0) window.close();
