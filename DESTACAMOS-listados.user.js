21505e18d299e8ce7a670327e141bfc4fd0b66c2
lumisjaeger <lumisjaeger>
2024-01-10 03:14:28 +0100

    DESTACAMOS: listados & perfil

---
+++ DESTACAMOS-listados.user.js
@@ -0,0 +1,12 @@
+/*
+==UserScript==
+@name           DESTACAMOS-listados
+@match          https://www.destacamos.com/*/telefono-*/listings.html
+@match          https://www.destacamos.com/telefono-*/listings.html
+@version        1.0
+==/UserScript==
+*/
+
+const phone = window.location.href.match(/.*?telefono-(\d{9})/)?.[1];
+
+window.location.replace('https://www.destacamos.com/keyword-' + phone + '/listings.html');
