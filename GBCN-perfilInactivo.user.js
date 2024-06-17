c3991f724c719c3364cc70bca308aa8dbc1e5b81
lumisjaeger <lumisjaeger>
2024-06-17 12:13:40 +0200

    GBCN-perfilInactivo: update disponibility check

--- GBCN-perfilInactivo.user.js
+++ GBCN-perfilInactivo.user.js
@@ -4,9 +4,9 @@
 @match          https://www.girlsmadrid.com/escort*
 @match          https://www.girlsbarcelona.com/escort*
 @match          https://www.girlsbcn.net/gb*
-@version        1.0
+@version        1.0.1
 @run-at         document_idle
 ==/UserScript==
 */
 
-if (document.querySelector("div.widget:nth-child(5) ul.meta-post li span:nth-child(2)")?.innerText !== 'Disponible') window.close();
+if (document.querySelector("div.widget:nth-child(5) ul.meta-post li span:nth-child(2)")?.innerText === 'NO DISPONIBLE') window.close();
