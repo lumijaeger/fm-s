bcca09fc2e9cf8aaa6bc678cdff47568d1c3786e
lumisjaeger <lumisjaeger>
2024-06-17 12:11:12 +0200

    SCOMPI: new scripts

---
+++ SLUMI-redirectScompi.user.js
@@ -0,0 +1,10 @@
+/*
+==UserScript==
+@name           SLUMI-redirecScompi
+@match          https://www.slumi.com/
+@match          https://www.slumi.com/*
+@version        1.0
+==/UserScript==
+*/
+
+window.location.replace(location.href.replace('slumi', 'scompi'));
