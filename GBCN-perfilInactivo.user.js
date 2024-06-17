cb751596fd8f635b618faa486bb1f6e5a41873a0
lumisjaeger <lumisjaeger>
2024-06-17 12:40:10 +0200

    GBCN-perfilInactivo: add new string to check

--- GBCN-perfilInactivo.user.js
+++ GBCN-perfilInactivo.user.js
@@ -1,12 +1,12 @@
-/*
-==UserScript==
-@name           GBCN-perfilInactivo
-@match          https://www.girlsmadrid.com/escort*
-@match          https://www.girlsbarcelona.com/escort*
-@match          https://www.girlsbcn.net/gb*
-@version        1.0.1
-@run-at         document_idle
-==/UserScript==
-*/
+// ==UserScript==
+// @name           GBCN-perfilInactivo
+// @match          https://www.girlsmadrid.com/escort*
+// @match          https://www.girlsbarcelona.com/escort*
+// @match          https://www.girlsbcn.net/gb*
+// @version        1.0.1
+// @run-at         document_idle
+// ==/UserScript==
 
-if (document.querySelector("div.widget:nth-child(5) ul.meta-post li span:nth-child(2)")?.innerText === 'NO DISPONIBLE') window.close();
+const availability = document.querySelector("div.widget:nth-child(5) ul.meta-post li span:nth-child(2)")?.innerText;
+
+if (availability === 'NO DISPONIBLE' || availability === 'PERFIL INACTIVO') window.close();
