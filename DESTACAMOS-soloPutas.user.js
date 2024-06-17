ed346c2e9822f56f0c1ae95b53ea25ec1c303d85
lumisjaeger <lumisjaeger>
2024-06-17 12:11:36 +0200

    DESTACAMOS-soloPutas: new script

---
+++ DESTACAMOS-soloPutas.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           DESTACAMOS-soloPutas
+// @match          https://www.destacamos.com/*/listings.html
+// @match          https://www.destacamos.net/*/listings.html
+// @version        1.0.0
+// ==/UserScript==
+
+[...document.querySelectorAll(
+  'div#featured div[aria-roledescription="slide"].item.splide__slide:has('
+  + 'div.box-image > :is(a[href*="travestis"], a[href*="chaperos"]))'
+), ...document.querySelectorAll(
+  'div#right_content > div[id]:has(:is(a[href*="travestis"], a[href*="chaperos"]))'
+)].forEach((e) => e.remove());
