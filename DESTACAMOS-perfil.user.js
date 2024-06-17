aed4b8c8de144310d38f4d1a7e2d61e4740d1f4e
lumisjaeger <lumisjaeger>
2024-06-17 12:15:23 +0200

    DESTACAMOS-perfil: add link buttons

--- DESTACAMOS-perfil.user.js
+++ DESTACAMOS-perfil.user.js
@@ -1,13 +1,13 @@
-/*
-==UserScript==
-@name           DESTACAMOS-perfil
-@match          https://www.destacamos.com/*/details.html
-@version        1.0
-==/UserScript==
-*/
+// ==UserScript==
+// @name           DESTACAMOS-perfil
+// @match          https://www.destacamos.com/*/details.html
+// @version        1.0.1
+// ==/UserScript==
+
 
 const phoneLink = document.querySelector('div#details div:first-child strong a');
 const googleLink = document.createElement('a');
+const allButton = document.createElement('button');
 const googleIcon = googleLink.appendChild(document.createElement('img'));
 
 googleLink.href = 'https://www.google.com/search?q=' + phoneLink.innerText;
@@ -17,23 +17,39 @@ googleIcon.src = "https://cdn.iconscout.com/icon/free/png-256/free-google-177222
 googleIcon.style = "height: 12px; margin-left: 5px"
 
 phoneLink.href =
-  'https://www.destacamos.com/keyword-'
-  + phoneLink.innerText
-  + '/listings.html';
+  'https://www.destacamos.com/keyword-' +
+  phoneLink.innerText +
+  '/listings.html';
 
-phoneLink.insertAdjacentElement('afterend', googleLink);
-
-/*
- * phoneLink.onclick = () => window.open(
-  'https://www.google.com/search?q=' + phoneLink.innerText,
-  '_blank'
+allButton.onclick = () => {
+  window.open(
+    'https://www.destacamos.com/keyword-' + phoneLink.innerText + '/listings.html',
+    '_blank'
   );
- *
- * phoneLink.onauxclick = (e) => {
+  window.open(
+    'https://www.google.com/search?q=' + phoneLink.innerText,
+    '_blank'
+  );
+  window.close();
+};
+
+allButton.onauxclick = (e) => {
+  e.preventDefault();
   if (e.button === 1) {
+    e.stopPropagation();
+    window.open(
+      'https://www.destacamos.com/keyword-' + phoneLink.innerText + '/listings.html',
+      '_blank'
+    );
     window.open(
       'https://www.google.com/search?q=' + phoneLink.innerText,
       '_blank'
     );
   }
-};*/
+};
+
+allButton.innerText = 'TODO';
+allButton.style = 'padding: 3px 4px 0px; border-radius: 8px; font-size: 12px'
+
+phoneLink.insertAdjacentElement('afterend', allButton);
+phoneLink.insertAdjacentElement('afterend', googleLink);
