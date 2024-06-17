4e63efc6b948c4f3236df0fce15796011eff3dbb
lumisjaeger <lumisjaeger>
2024-06-17 12:17:45 +0200

    DESTACAMOS-busquedaVacia: remove unneeded variable

--- DESTACAMOS-busquedaVacia.user.js
+++ DESTACAMOS-busquedaVacia.user.js
@@ -1,14 +1,12 @@
-/*
-==UserScript==
-@name           DESTACAMOS-busquedaVacia
-@match          http://www.destacamos.com/keyword-*/listings.html
-@version        1.0
-==/UserScript==
-*/
+// ==UserScript==
+// @name           DESTACAMOS-busquedaVacia
+// @match          http://www.destacamos.com/keyword-*/listings.html
+// @version        1.0.1
+// ==/UserScript==
+
 
 let targetPhone = window.location.href.match('\\d{9}')[0];
 let phones = document.querySelectorAll('div.detalles span b');
-let found = false;
 
 if (!(Array.from(phones).some(tel => tel.innerText === targetPhone))) {
   window.close();
