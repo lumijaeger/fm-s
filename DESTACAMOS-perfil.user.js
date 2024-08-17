fdaed29efd572d00fd974eeb451c531721c55b4f
lumisjaeger <lumisjaeger>
2024-08-17 06:52:23 +0200

    DESTACAMOS-perfil: add copyToClipboard and fix third mouse button click

--- DESTACAMOS-perfil.user.js
+++ DESTACAMOS-perfil.user.js
@@ -2,7 +2,8 @@
 // @name           DESTACAMOS-perfil
 // @match          https://www.destacamos.com/*/details.html
 // @match          https://www.destacamos.net/*/details.html
-// @version        1.0.2
+// @require        UtilitiesPack
+// @version        1.1
 // ==/UserScript==
 
 const phoneLink = document.querySelector('div#details div:first-child strong a');
@@ -12,16 +13,19 @@ const googleIcon = googleLink.appendChild(document.createElement('img'));
 
 googleLink.href = 'https://www.google.com/search?q=' + phoneLink.innerText;
 googleLink.target = '_blank';
+googleLink.onclick = () => copyToClipboard(phoneLink.innerText);
 
-googleIcon.src = "https://cdn.iconscout.com/icon/free/png-256/free-google-1772223-1507807.png"
-googleIcon.style = "height: 12px; margin-left: 5px"
+googleIcon.src = "https://cdn.iconscout.com/icon/free/png-256/free-google-1772223-1507807.png";
+googleIcon.style = "height: 12px; margin-left: 5px";
 
 phoneLink.href =
   'https://www.destacamos.com/keyword-' +
   phoneLink.innerText +
   '/listings.html';
+phoneLink.onclick = () => copyToClipboard(phoneLink.innerText);
 
-allButton.onclick = () => {
+allButton.addEventListener('click', () => {
+  copyToClipboard(phoneLink.innerText);
   window.open(
     'https://www.destacamos.com/keyword-' + phoneLink.innerText + '/listings.html',
     '_blank'
@@ -31,11 +35,16 @@ allButton.onclick = () => {
     '_blank'
   );
   window.close();
-};
+});
 
-allButton.onauxclick = (e) => {
-  e.preventDefault();
+allButton.addEventListener('mousedown', (e) => {
+  if (e.button === 1) e.preventDefault();
+});
+
+allButton.addEventListener('auxclick', (e) => {
   if (e.button === 1) {
+    copyToClipboard(phoneLink.innerText);
+    e.preventDefault();
     e.stopPropagation();
     window.open(
       'https://www.destacamos.com/keyword-' + phoneLink.innerText + '/listings.html',
@@ -46,10 +55,10 @@ allButton.onauxclick = (e) => {
       '_blank'
     );
   }
-};
+});
 
 allButton.innerText = 'TODO';
-allButton.style = 'padding: 3px 4px 0px; border-radius: 8px; font-size: 12px'
+allButton.style = 'padding: 3px 4px 0px; border-radius: 8px; font-size: 12px';
 
 phoneLink.insertAdjacentElement('afterend', allButton);
 phoneLink.insertAdjacentElement('afterend', googleLink);
