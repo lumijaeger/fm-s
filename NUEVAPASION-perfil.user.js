48f5ecbb3439b244b115f28e153007efa9442841
lumisjaeger <lumisjaeger>
2024-08-19 13:06:20 +0200

    NUEVAPASION-perfil: new script

---
+++ NUEVAPASION-perfil.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           NUEVAPASION-perfil
+// @match          https://nuevapasion.com/anuncio/*
+// @version        1.0.0
+// @require        UtilitiesPack
+// ==/UserScript==
+
+replaceAllPhoneLinksWithGoogle();
