b8688cd80876281ae1a967a7bff0560bba101a51
lumisjaeger <lumisjaeger>
2024-12-23 02:19:26 +0100

    DESTACAMOSTOP-perfil: add script

---
+++ DESTACAMOSTOP-perfil.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           DESTACAMOSTOP-perfil
+// @updateURL      https://raw.githubusercontent.com/lumisjaeger/fm-s/refs/heads/master/DESTACAMOSTOP-perfil.user.js
+// @match          https://destacamostop.com/locuo/*
+// @require        UtilitiesPack
+// @version        1.0
+// ==/UserScript==
+
+replaceAllPhoneLinksWithGoogle();
