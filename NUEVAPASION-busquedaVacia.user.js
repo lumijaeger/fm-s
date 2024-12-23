b5b21839633972246a894ccd694e83ce984037f5
lumisjaeger <lumisjaeger>
2024-12-23 03:06:53 +0100

    NUEVAPASION-busquedaVacia: update selector

--- NUEVAPASION-busquedaVacia.user.js
+++ NUEVAPASION-busquedaVacia.user.js
@@ -2,8 +2,8 @@
 // @name           NUEVAPASION-busquedaVacia
 // @updateURL      https://raw.githubusercontent.com/lumisjaeger/fm-s/refs/heads/master/NUEVAPASION-busquedaVacia.user.js
 // @match          https://nuevapasion.com/anuncios-escort-para-hombre/buscar-*
-// @version        1.0
+// @version        1.0.1
 // ==/UserScript==
 
-if (document.querySelector("div#body > div > section > article:last-of-type"))
+if (document.querySelector("main > article:first-of-type:last-child"))
   window.close();
