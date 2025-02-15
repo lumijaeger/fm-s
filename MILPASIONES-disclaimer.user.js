cab82ce0daee7a07d3ad923e72f3367131069616
lumisjaeger <lumisjaeger>
2025-02-15 03:47:07 +0100

    MILPASIONES-disclaimer

---
+++ MILPASIONES-disclaimer.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           MILPASIONES-disclaimer
+// @match          https://milpasiones.com/*
+// @updateURL      https://raw.githubusercontent.com/lumijaeger/fm-s/refs/heads/master/MILESCORTS-busquedaVacia.user.js
+// @version        1.0
+// ==/UserScript==
+
+document.getElementsByClassName("disclaimer")[0].remove();
